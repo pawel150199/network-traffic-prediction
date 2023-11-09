ed95b2085dcc03d6ac3d7d608b6c51414bafe72a
pawel150199 <pawel.polski99@gmail.com>
2023-11-09 16:45:52 +0100

    add us26 dataset

--- graph_visualizations.py
+++ graph_visualizations.py
@@ -1,5 +1,5 @@
 from visualize_graph import vizualization
 
-path_to_data = "Euro28/request-set_0/requests.csv"
-save_path = "img/graph.png"
+path_to_data = "US26/request-set_0/requests.csv"
+save_path = "img/us26_graph.png"
 vizualization(path_to_data=path_to_data, save_path=save_path)
