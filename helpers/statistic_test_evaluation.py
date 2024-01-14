f032b9073643ff97f43d52f4d68405d378df841a
Michal170 <93790482+Michal170@users.noreply.github.com>
2024-01-14 17:58:22 +0100

    fs tables

--- helpers/statistic_test_evaluation.py
+++ helpers/statistic_test_evaluation.py
@@ -12,57 +12,84 @@ Class generate tables with paired statistic tests and store it
 """
 
 
-class StatisticTest():
+class StatisticTest:
     def __init__(self, evaluator):
         self.evaluator = evaluator
 
-    def process(self, table_name, alpha=0.05, m_fmt="%3f", std_fmt=None, nc="---", db_fmt="%s", tablefmt="plain"):
+    def process(
+        self,
+        table_name,
+        alpha=0.05,
+        m_fmt="%3f",
+        std_fmt=None,
+        nc="---",
+        db_fmt="%s",
+        tablefmt="plain",
+    ):
         """Process"""
 
         # Ignore warnings
         warnings.filterwarnings("ignore")
 
         try:
-            # PARAM X FOLD X CLASSIFIER X METRIC 
+            # PARAM X FOLD X CLASSIFIER X METRIC
             scores = self.evaluator.scores
             mean_scores = self.evaluator.mean
             std = self.evaluator.std
             clfs = list(self.evaluator.clfs.keys())
-            parameters = ["highestSlot", "avgHighestSlot", "sumOfSlots", "avgActiveTransceivers"]
+            parameters = [
+                "highestSlot",
+                "avgHighestSlot",
+                "sumOfSlots",
+                "avgActiveTransceivers",
+            ]
             n_clfs = len(clfs)
             t = []
-            # Generate tables 
-            
+            # Generate tables
+
             for param_idx, param_name in enumerate(parameters):
                 # Mean value
-                t.append(['']+[db_fmt % param_name] + ["%.3f" % v for v in mean_scores[param_idx, :]])
+                t.append(
+                    [""]
+                    + [db_fmt % param_name]
+                    + ["%.3f" % v for v in mean_scores[param_idx, :]]
+                )
                 # If std_fmt is not None, std will appear in tables
                 if std_fmt:
-                    t.append([''] + [std_fmt % v for v in std[param_idx, :]])
+                    t.append([""] + [std_fmt % v for v in std[param_idx, :]])
                 # Calculate T and P for T-studenta test
                 T, p = np.array(
-                    [[ttest_ind(scores[param_idx, :, i],
-                        scores[param_idx, :, j])
-                    for i in range(len(clfs))]
-                    for j in range(len(clfs))]
+                    [
+                        [
+                            ttest_ind(scores[param_idx, :, i], scores[param_idx, :, j])
+                            for i in range(len(clfs))
+                        ]
+                        for j in range(len(clfs))
+                    ]
                 ).swapaxes(0, 2)
                 _ = np.where((p < alpha) * (T > 0))
-                conclusions = [list(1 + _[1][_[0] == i])
-                            for i in range(n_clfs)]
-        
-                t.append([''] + [''] + [", ".join(["%i" % i for i in c])
-                                if len(c) > 0 and len(c) < len(clfs)-1 else ("all" if len(c) == len(clfs)-1 else nc)
-                                for c in conclusions])
+                conclusions = [list(1 + _[1][_[0] == i]) for i in range(n_clfs)]
 
-            # Show outputs  
-            headers = ['Parameter']
+                t.append(
+                    [""]
+                    + [""]
+                    + [
+                        ", ".join(["%i" % i for i in c])
+                        if len(c) > 0 and len(c) < len(clfs) - 1
+                        else ("all" if len(c) == len(clfs) - 1 else nc)
+                        for c in conclusions
+                    ]
+                )
+
+            # Show outputs
+            headers = ["Parameter"]
             for i in clfs:
                 headers.append(i)
             print(tabulate(t, headers))
 
             # Save outputs as .tex extension
-            with open('tables/%s.txt' % (table_name), 'w') as f:
-                    f.write(tabulate(t, headers, tablefmt='latex'))
+            with open("tables/%s.txt" % (table_name), "w") as f:
+                f.write(tabulate(t, headers, tablefmt="latex"))
 
         except ValueError:
-            error('Incorrect value!')
+            error("Incorrect value!")
