b8809ed406aac800efe5e1e1001c532051976e7a
pawel150199 <pawel.polski99@gmail.com>
2024-02-03 07:50:31 +0100

    add results

--- LSTM.py
+++ LSTM.py
@@ -9,67 +9,43 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 from keras.callbacks import EarlyStopping, ModelCheckpoint
 
-import warnings
-
 from helpers.import_data import import_data
 from helpers.metrics import accuracy
+import warnings
+
 
 warnings.filterwarnings("ignore")
-
-data, labels = import_data("Euro28")
-#data = data.reshape(100,300)
-
-print(data.shape)
-
 tf.config.set_visible_devices([], 'GPU')
 
-
+data, labels = import_data("Euro28")
 results = labels[:, 1]
+#data = data.reshape(100,300)
 
 X_train, X_test, y_train, y_test = train_test_split(data, results, test_size=0.2, random_state=1410)
 
-model = Sequential([layers.Input((100, 3)),
-                    layers.LSTM(128),
-                    layers.Dense(256, activation="relu"),
-                    layers.Dense(256, activation="relu"),
-                    layers.Dense(512, activation="relu"),
-                    layers.Dense(256, activation="relu"),
-                    layers.Dense(128, activation="relu"),
-                    layers.Dense(64, activation="relu"),
-                    layers.Dense(32, activation="relu"),
-                    layers.Dense(1)])
+#model = Sequential([layers.Input((100, 3)),
+#                    layers.LSTM(128),
+#                    layers.Dense(256, activation="relu"),
+#                    layers.Dense(256, activation="relu"),
+#                    layers.Dense(512, activation="relu"),
+#                    layers.Dense(256, activation="relu"),
+#                    layers.Dense(128, activation="relu"),
+#                    layers.Dense(64, activation="relu"),
+#                    layers.Dense(32, activation="relu"),
+#                    layers.Dense(1)])
 
 model = Sequential([layers.Input((100, 3)),
                     layers.LSTM(21),
                     layers.Dense(20)])
-#LSTM(21)
-#DENSE(len(features))
 
 model.compile(loss="mse",
             optimizer=Adam(learning_rate=0.001),
             metrics=['mean_absolute_percentage_error'])
 
-earlyStopping = EarlyStopping(monitor='val_loss',
-                            patience=3,
-                            verbose=1)
-
-modelCheckpoint = ModelCheckpoint(filepath="best_lstm.h5",
-                                monitor='val_loss',
-                                save_best_only=True)
     
 history = model.fit(X_train, y_train, epochs=200, validation_data=(X_test, y_test))
-
 pred = model.predict(X_test).flatten()
-print(pred.shape)
-
-from helpers.metrics import accuracy
-
 mae = mean_absolute_percentage_error(y_test, pred)
-
-
-
-print(f"\n\nPredicted values: {pred}")
-print(f"Real values: {y_test}")
 
 plt.figure(figsize=(12,7))
 plt.plot(pred, linewidth=0.8, color="red", label="Real Values")
